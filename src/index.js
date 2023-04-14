16c6239972811c4fc4917ee0010b79f424a869e6
Himanshu Rajput <hiowner00@gmail.com>
2023-04-14 18:37:02 +0530

    subsribe to store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { legacy_createStore as createStore} from 'redux'
+import movies from './reducers';
 
+const store = createStore(movies)
+// console.log(store.getState())
+
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies : [{name: "superman"}]
+// })
+// console.log(store.getState())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store ={store} />
   </React.StrictMode>
 );
 
