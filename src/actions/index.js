792d6bc45402b4383c461947336dc8d756795ed2
Himanshu Rajput <hiowner00@gmail.com>
2023-04-14 23:47:16 +0530

    added favourite in state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
-{
-    type: 'ADD_MOVIES'
-    movies : [m1,m2,m3]
-}
+// {
+//     type: 'ADD_MOVIES'
+//     movies : [m1,m2,m3]
+// }
+ export const ADD_MOVIES = 'ADD_MOVIES'
+
+ export function addMovies (movies){
+    return {
+        type: ADD_MOVIES,
+        movies: movies
+      }
+ }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import React from 'react';
+import { addMovies } from '../actions';
 
 class App extends React.Component {
 
@@ -10,14 +11,11 @@ class App extends React.Component {
     store.subscribe(()=>{
       this.forceUpdate();
     })
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    })
+    store.dispatch(addMovies(data))
     console.log(this.props.store.getState())
   }
   render(){
-    const movies = this.props.store.getState();
+    const {list} = this.props.store.getState();
     return (
       <div className="App">
         <Navbar />
@@ -28,7 +26,7 @@ class App extends React.Component {
           </div>
   
           <div className="lists">
-            {movies.map((movie,index) => (
+            {list.map((movie,index) => (
               <MovieCard movie = {movie} key={`movies-${index}`}/>
             ))}
           </div>
